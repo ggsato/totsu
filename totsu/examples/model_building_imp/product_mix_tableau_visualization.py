dc9b7e4e00d20b3b854c3a3e6bc3e4764793fa2f
Takenori Sato <takenori.sato@gmail.com>
2024-12-06 04:08:55 +0900

    #27 added --use_dual

--- totsu/examples/model_building_imp/product_mix_tableau_visualization.py
+++ totsu/examples/model_building_imp/product_mix_tableau_visualization.py
@@ -1,16 +1,19 @@
-from .product_mix import create_model
+from .product_mix import create_model, create_dual_model
 from ...utils.tableau_visualizer import TableauVisualizer
 from ...core.super_simplex_solver import SuperSimplexSolver
 
-def main():
-    # Create the primal model
-    primal_model = create_model()
+def main(use_dual):
+    # Create the model
+    if use_dual:
+        model = create_dual_model()
+    else:
+        model = create_model()
 
     # Initialize the solver
     solver = SuperSimplexSolver()
 
     # Visualize the solution
-    visualizer = TableauVisualizer(primal_model, solver)
+    visualizer = TableauVisualizer(model, solver)
     try:
         visualizer.solve_model()
         visualizer.show_tableau_visualization()
@@ -18,4 +21,8 @@ def main():
         print("Failed to solve the model and show the visualization")
 
 if __name__ == "__main__":
-    main()
+    import argparse
+    parser = argparse.ArgumentParser(description="A program to show Tableau Visualizer of product mix models")
+    parser.add_argument("--use_dual", action='store_true', help="If set, use the dual model, the primal model otherwise.")
+    args = parser.parse_args()
+    main(args.use_dual)
--- totsu/examples/model_building_imp/product_mix_visualization.py
+++ totsu/examples/model_building_imp/product_mix_visualization.py
@@ -1,18 +1,25 @@
 from pyomo.environ import SolverFactory
-from .product_mix import create_model
+from .product_mix import create_model, create_dual_model
 from ...utils.sensitivity_analyzer import SensitivityAnalyzer
 from ...core.totsu_simplex_solver import TotsuSimplexSolver # required to register
 
-def main():
-    # Create the primal model
-    primal_model = create_model()
+def main(use_dual):
+    # Create the model
+    if use_dual:
+        model = create_dual_model()
+    else:
+        model = create_model()
     solver = SolverFactory("totsu")
 
-    visualizer = SensitivityAnalyzer(primal_model, solver)
+    visualizer = SensitivityAnalyzer(model, solver)
     if visualizer.solve_primal():
         visualizer.show_analyzer()
     else:
         print("Failed to solve the primal model.")
 
 if __name__ == "__main__":
-    main()
+    import argparse
+    parser = argparse.ArgumentParser(description="A program to show Visualizer of product mix models")
+    parser.add_argument("--use_dual", action='store_true', help="If set, use the dual model, the primal model otherwise.")
+    args = parser.parse_args()
+    main(args.use_dual)
