c29ed11266cfd00c8b265485e3be732fafafc834
Takenori Sato <takenori.sato@gmail.com>
2024-12-10 16:10:49 +0900

    #10 Added Three-industry Economy Example

---
+++ totsu/examples/model_building_imp/examples/5-1_three_industry_economy/three_industry_economy_visualization.py
@@ -0,0 +1,18 @@
+from pyomo.environ import SolverFactory
+from .three_industry_economy import create_alternative_model
+from .....utils.sensitivity_analyzer import SensitivityAnalyzer
+from .....core.totsu_simplex_solver import TotsuSimplexSolver # required to register
+
+def main():
+    # Create the model
+    model = create_alternative_model()
+    solver = SolverFactory("totsu")
+
+    visualizer = SensitivityAnalyzer(model, solver)
+    if visualizer.solve_primal():
+        visualizer.show_analyzer()
+    else:
+        print("Failed to solve the primal model.")
+
+if __name__ == "__main__":
+    main()
