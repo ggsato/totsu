34ae6893ea5f689fa693d8724fe6be3beef50e98
Takenori Sato <takenori.sato@gmail.com>
2024-12-16 13:09:42 +0900

    #39 Updated minimum_cost_flow.py

--- totsu/examples/model_building_imp/examples/5-4_minimum_cost_flow/minimum_cost_flow.py
+++ totsu/examples/model_building_imp/examples/5-4_minimum_cost_flow/minimum_cost_flow.py
@@ -54,102 +54,11 @@ def create_model():
 
     return model
 
-def create_minimum_model():
-    # Create a Pyomo model
-    model = ConcreteModel()
-
-    min_nodes = [i for i in range(3)]
-    min_costs = {(0, 1): 1, (0, 2): 3, (1, 2): 1} # 0 -> 1 -> 2: 1 + 1 = 2, 0 -> 2: 3
-    min_availabilities = {0: 2}
-    min_requirements = {2: -2}
-
-    # Sets
-    model.nodes = Set(initialize=min_nodes)
-    model.arcs = Set(initialize=[(i, j) for (i, j), c in min_costs.items() if c is not None])
-
-    # Parameters
-    model.costs = Param(model.arcs, initialize={k: v for k, v in min_costs.items() if v is not None})
-    model.availabilities = Param(model.nodes, initialize=min_availabilities, default=0)
-    model.requirements = Param(model.nodes, initialize=min_requirements, default=0)
-
-    # Variables
-    model.flow = Var(model.arcs, within=NonNegativeReals)
-
-    # Constraints
-    def flow_balance_rule(model, node):
-        inflow = sum(model.flow[i, node] for i in model.nodes if (i, node) in model.arcs)
-        outflow = sum(model.flow[node, j] for j in model.nodes if (node, j) in model.arcs)
-        return inflow +  model.availabilities[node] ==  outflow + (-1 * model.requirements[node])
-
-    model.flow_balance = Constraint(model.nodes, rule=flow_balance_rule)
-
-    # Objective: Minimize the total cost
-    def objective_rule(model):
-        return summation(model.costs, model.flow)
-
-    model.objective = Objective(rule=objective_rule, sense=minimize)
-
-    return model
-
-def create_minimum_model2():
-    # Create a Pyomo model
-    model = ConcreteModel()
-
-    min_nodes = [i for i in range(4)]
-    min_costs = {(0, 1): 1, (0, 2): 3, (1, 2): 1, (2,3): 1} # 0 -> 1 -> 2: 1 + 1 = 2, 0 -> 2: 3
-    min_availabilities = {0: 2}
-    min_requirements = {3: -2}
-
-    # Sets
-    model.nodes = Set(initialize=min_nodes)
-    model.arcs = Set(initialize=[(i, j) for (i, j), c in min_costs.items() if c is not None])
-
-    # Parameters
-    model.costs = Param(model.arcs, initialize={k: v for k, v in min_costs.items() if v is not None})
-    model.availabilities = Param(model.nodes, initialize=min_availabilities, default=0)
-    model.requirements = Param(model.nodes, initialize=min_requirements, default=0)
-
-    # Variables
-    model.flow = Var(model.arcs, within=NonNegativeReals)
-
-    # Constraints
-    def flow_balance_rule(model, node):
-        inflow = sum(model.flow[i, node] for i in model.nodes if (i, node) in model.arcs)
-        outflow = sum(model.flow[node, j] for j in model.nodes if (node, j) in model.arcs)
-        return inflow +  model.availabilities[node] ==  outflow + (-1 * model.requirements[node])
-
-    model.flow_balance = Constraint(model.nodes, rule=flow_balance_rule)
-
-    # Objective: Minimize the total cost
-    def objective_rule(model):
-        return summation(model.costs, model.flow)
-
-    model.objective = Objective(rule=objective_rule, sense=minimize)
-
-    return model
-
-def create_minimal_fixed_variable_model():
-    model = ConcreteModel()
-
-    # Define variables
-    model.flow_1 = Var(within=NonNegativeReals)
-    model.flow_2 = Var(within=NonNegativeReals, bounds=(2.0, 2.0))  # Fixed via bounds
-
-    # Define constraint
-    model.eq = Constraint(expr=model.flow_1 + model.flow_2 == 2)
-
-    # Define objective
-    model.objective = Objective(expr=model.flow_1, sense=minimize)
-
-    return model
-
-if __name__ == "__main__":
+def main(use_glpk):
     import sys, traceback
     try:
-        print("solving the model")
-        use_glpk = False
-        model = create_minimum_model2()
-        model.pprint()
+        print(f"solving the model - use_glpk = {use_glpk}")
+        model = create_model()
         if use_glpk:
             solver = SolverFactory("glpk")
         else:
@@ -166,9 +75,16 @@ if __name__ == "__main__":
                     print(f"Flow from {i} to {j}: {model.flow[i, j].value}")
         else:
             print(f"tableau objective value = {solver.get_current_objective_value()}")
-            print(f"final objective value = {model.objective()}")
+            print(f"final objective value = {solver.get_objective_value()}")
             for var in solution:
                 print(f"{var}: {solution[var]}")
 
     except Exception as ex:
         traceback.print_exception(ex)
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description="Minimum Cost Flow")
+    parser.add_argument("--use_glpk", action='store_true', help="If set, use the glpk solver, SuperSimplexSolver otherwise.")
+    args = parser.parse_args()
+    main(args.use_glpk)
